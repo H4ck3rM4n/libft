26cda6455ab72bd5759e5cd569a6b2c960d8582e
H4ck3rM4n <apavalac@student.42.fr>
2019-09-06 08:26:40 +0000

    list rev added

--- include/libft.h
+++ include/libft.h
@@ -80,6 +80,7 @@ void    ft_lstdelone(t_list **alst, void (*del)(void*, size_t));
 void    ft_lstadd(t_list **alst, t_list *new);
 t_list  *ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
 void    ft_lstdel(t_list **alst, void (*del)(void *, size_t));
+t_list   *ft_lstrev(t_list *lst);
 
 int     get_next_line(int fd, char **line);
 
---
+++ src/ft_lstrev.c
@@ -0,0 +1,13 @@
+#include "libft.h"
+
+t_list      *ft_lstrev(t_list *lst)
+{
+    t_list *new = ft_lstnew(0, 0);
+
+    while(lst)
+    {
+        ft_lstadd(&new, lst);
+        lst = lst->next;
+    }
+    return (new);
+}
